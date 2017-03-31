82edfddf3d5b5d2aa80ba7c1e815039ae939642a
mengyang <1456663919@qq.com>
2017-03-31 20:30:56 +0800

    DOM->utils

--- base.js
+++ base.js
@@ -1,6 +1,6 @@
-var DOM={};//命名空间，名子空间，单例模式
+var utils={};//命名空间，名子空间，单例模式
 //获得ele元素的索引值
-DOM.getIndex=function (ele){
+utils.getIndex=function (ele){
     var index=0;
     var prev=ele.previousSibling;
     while(prev){
@@ -13,7 +13,7 @@ DOM.getIndex=function (ele){
 }
 //1==true; 0=="";false==0;
 //获得所有兄弟元素
-DOM.siblings=function(ele){
+utils.siblings=function(ele){
     var nodes=ele.parentNode.childNodes;
     var a=[];
     for(var i = 0;i<nodes.length;i++){
@@ -26,7 +26,7 @@ DOM.siblings=function(ele){
     return a;
 }
 //获得所有的哥哥元素节点
-DOM.prevSiblings=function(ele){
+utils.prevSiblings=function(ele){
     /*var nodes=ele.parentNode.childNodes;
      var a=[];
      for(var i=0;i<nodes.length;i++){
@@ -51,7 +51,7 @@ DOM.prevSiblings=function(ele){
     return a;
 }
 //获得所有的弟弟节点
-DOM.nextSiblings=function(ele){
+utils.nextSiblings=function(ele){
     /*var nodes=ele.parentNode.childNodes;
      var a=[];
      for(var i=nodes.length-1;i>=0;i--){
@@ -73,8 +73,8 @@ DOM.nextSiblings=function(ele){
     return a;
 }
 //获得ele的相邻的第一个元素弟弟节点
-DOM.next=function(ele){
-    /*var next=DOM.nextSiblings(ele)[0];
+utils.next=function(ele){
+    /*var next=utils.nextSiblings(ele)[0];
      return next?next:null;//保障人品的*/
     if(ele.nextElementSibling){
         return ele.nextElementSibling;//
@@ -89,7 +89,7 @@ DOM.next=function(ele){
     }
 }
 //获得相邻的第一个元素哥哥节点
-DOM.prev=function(ele){
+utils.prev=function(ele){
     if(ele.previousElementSibling){
         return ele.previousElementSibling;
     }
@@ -102,7 +102,7 @@ DOM.prev=function(ele){
     }
 }
 //获得parent的元素子节点
-DOM.children=function(parent,strTagName){
+utils.children=function(parent,strTagName){
 //如果有第二个参数，表示获得指得标签的子元素（第二个参数可选）
     var nodes=parent.childNodes;
     var a=[];
@@ -128,7 +128,7 @@ DOM.children=function(parent,strTagName){
 }
 
 //处理兼容后的获取类名节点
-DOM.getElesByClass=function (strClass){
+utils.getElesByClass=function (strClass){
     strClass=strClass.replace(/^ +| +$/g,"");
     var aClass=strClass.split(/ +/);
     var a=document.getElementsByTagName("*");
@@ -147,13 +147,13 @@ DOM.getElesByClass=function (strClass){
     return a;
 }
 //添加类名
-DOM.addClass=function(ele,strClass){
+utils.addClass=function(ele,strClass){
     var reg=new RegExp("(?:^| )"+strClass+"(?: |$)");
     if(!reg.test(ele.className))
         ele.className+=" "+strClass;
 }
 //移除类名
-DOM.removeClass=function(ele,strClass){
+utils.removeClass=function(ele,strClass){
     var reg=new RegExp("(?:^| )"+strClass+"(?: |$)","g");
     ele.className=ele.className.replace(reg,"");
 
@@ -162,7 +162,7 @@ DOM.removeClass=function(ele,strClass){
 //F11逐语句，F8逐断点，F10逐方法（就是当前代码里如果有方法，则跳过此方法）
 
 //类数组（arguments）转化为数组
-DOM.listToArray=function(likeArray){
+utils.listToArray=function(likeArray){
     var ary = [];
     try {
         ary = Array.prototype.slice.call(likeArray, 0);
@@ -174,27 +174,27 @@ DOM.listToArray=function(likeArray){
     return ary;
 };
 //获得第一个儿子元素
-DOM.firstChild=function (curEle, tagName) {
+utils.firstChild=function (curEle, tagName) {
     var children = this.children(curEle, tagName);
     return children.length > 0 ? children[0] : null;
 };
 //获得最后一个儿子元素
-DOM.lastChild = function (curEle, tagName) {
+utils.lastChild = function (curEle, tagName) {
     var children = this.children(curEle, tagName);
     return children.length > 0 ? children[children.length - 1] : null;
 };
 //把新节点插入为父节点的首个节点
-DOM.prepend=function (newEle, container) {
+utils.prepend=function (newEle, container) {
     var firstEle = this.firstChild(container);
     firstEle ? container.insertBefore(newEle, firstEle) : container.appendChild(newEle);
 };
 //把新节点插入到老节点之前第一个元素节点
-DOM.insertAfter=function (newEle, oldEle) {
+utils.insertAfter=function (newEle, oldEle) {
     var next = this.next(oldEle), parent = oldEle.parentNode;
     next ? parent.insertBefore(newEle, next) : parent.appendChild(newEle);
 };
 //获得一个元素节点的属性（没有属性值，返回属性值；有属性值，设置属性值）
-DOM.attr=function (curEle, property, value) {
+utils.attr=function (curEle, property, value) {
     if (typeof value === "undefined") {
         return curEle[property];
     } else {
@@ -202,11 +202,11 @@ DOM.attr=function (curEle, property, value) {
     }
 };
 //处理兼容，获取body的属性值
-DOM.getWin = function (attr) {
+utils.getWin = function (attr) {
     return document.documentElement[attr] || document.body[attr];
 };
 //处理兼容，获取元素距离body的高或宽
-DOM.offset = function (curEle) {
+utils.offset = function (curEle) {
     var par = curEle.offsetParent, top = 0, left = 0;
     top += curEle.offsetTop;
     left += curEle.offsetLeft;
